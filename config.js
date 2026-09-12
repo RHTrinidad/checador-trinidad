a0b7904c4ac67183e220e6537ee93a7315907b05
RHTrinidad <costostrinidad@gmail.com>
2026-09-11 19:00:48 -0600

    Refactor config.js structure and update keys

--- config.js
+++ config.js
@@ -1,32 +1,17 @@
 module.exports = {
-  sucursales: {
-    bucareli: { 
-      lat: 19.4314119, 
-      lng: -99.1512074, 
-      nombre: 'BUCARELI 66',
-      radioEntrada: 150,
-      radioSalidaAviso: 100,
-      radioSalidaLimite: 150
-    },
-    coyoacan: { 
-      lat: 19.3526359, 
-      lng: -99.1808071, 
-      nombre: 'COYOACAN MERCADO 86',
-      radioEntrada: 150,
-      radioSalidaAviso: 100,
-      radioSalidaLimite: 150
-    }
+  Administradores: ["5215512345678@s.whatsapp.net"],
+  Sucursales: {
+    bucareli: { lat: 19.4314119, lng: -99.1512074, nombre: "BUCARELI 66", radioEntrada: 150, radioSalidaAviso: 100, radioSalidaLimite: 150 },
+    coyoacan: { lat: 19.3526359, lng: -99.1808071, nombre: "COYOACAN MERCADO 86", radioEntrada: 150, radioSalidaAviso: 100, radioSalidaLimite: 150 }
   },
-  administradores: ['120363412984528459@g.us'],
-  jornada: {
-    inicio: 5, // 5am
-    descanso: 1 // 1 = Lunes
+  GruposSucursales: {
+    "120363000000000000@g.us": "coyoacan",
+    "120363000000000001@g.us": "bucareli"
   },
-  mensajes: {
-    fueraRangoEntrada: '❌ La entrada se registra en la sucursal, intenta de nuevo en la unidad',
-    fueraRangoSalida: '❌ La salida se registra en la sucursal, intenta de nuevo en la unidad',
-    avisoSalida: 'La salida es a 100 metros de la sucursal',
-    graciasEntrada: '✅ Gracias, buen turno!',
-    graciasSalida: '✅ Gracias!'
+  Config: {
+    jornadaInicioHora: 5,
+    descansoObligatorio: 1,
+    reporteDia: 1,
+    reporteHora: 8
   }
 };
