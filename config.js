f48a5a505f6b9a13764bd45eeec56376c25c1a78
RHTrinidad <costostrinidad@gmail.com>
2026-09-11 19:09:21 -0600

    Refactor config.js structure and remove unused sections
    Removed Administradores and GruposSucursales sections. Moved Sucursales into Config.

--- config.js
+++ config.js
@@ -1,17 +1,6 @@
 module.exports = {
-  Administradores: ["5215512345678@s.whatsapp.net"],
   Sucursales: {
-    bucareli: { lat: 19.4314119, lng: -99.1512074, nombre: "BUCARELI 66", radioEntrada: 150, radioSalidaAviso: 100, radioSalidaLimite: 150 },
-    coyoacan: { lat: 19.3526359, lng: -99.1808071, nombre: "COYOACAN MERCADO 86", radioEntrada: 150, radioSalidaAviso: 100, radioSalidaLimite: 150 }
-  },
-  GruposSucursales: {
-    "120363000000000000@g.us": "coyoacan",
-    "120363000000000001@g.us": "bucareli"
-  },
-  Config: {
-    jornadaInicioHora: 5,
-    descansoObligatorio: 1,
-    reporteDia: 1,
-    reporteHora: 8
+    bucareli: { lat: 19.4314119, lng: -99.1512074, nombre: "BUCARELI 66" },
+    coyoacan: { lat: 19.3526359, lng: -99.1808071, nombre: "COYOACAN MERCADO 86" }
   }
 };
